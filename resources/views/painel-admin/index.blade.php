2d0345bdcac681f1445cd0f7017094bf2bc7efa0
beta <buenocostago@hotmail.com>
2023-06-25 15:34:35 -0300

    primeira versão/continuar aula16

---
+++ resources/views/painel-admin/index.blade.php
@@ -0,0 +1,4 @@
+@extends('template.painel-admin')
+@section('content')
+Home do Admin
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\CadInstrutoresController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', HomeController::class)->name('home'); 
+Route::post('painel', [UsuarioController::class, 'login'])->name('usuarios.login'); 
+Route::post('intrutores', [CadInstrutoresController::class, 'index'])->name('instrutores.index'); 
+
+    
